649df4b0fe1fae713b92c95cd248b50c3b5087a5
machikurada <machikurada@gmail.com>
2026-04-09 17:25:25 +0200

    created and tested block to block type

--- src/mardown_to_blocks.py
+++ src/mardown_to_blocks.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 def markdown_to_blocks(markdown):
     raw_blocks = markdown.split("\n\n")
     new_blocks = []
@@ -5,3 +8,36 @@ def markdown_to_blocks(markdown):
         if len(block.strip()) > 0:
             new_blocks.append(block.strip())
     return new_blocks
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+def block_to_block_type(text):
+    lines = text.split("\n")
+    
+    if text.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    
+    if text.startswith("```\n") and text.endswith("```") and len(lines)>=2:
+        return BlockType.CODE
+
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    
+    for i, line in enumerate(lines, start=1):
+        is_ordered_list = True
+        if not line.startswith(f"{i}. "):
+            is_ordered_list = False
+    if is_ordered_list:
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
+
--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from mardown_to_blocks import markdown_to_blocks
+from mardown_to_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMardownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -77,3 +77,72 @@ This just one big block with **bold** and _italics_ and a [link](https://www.url
 """
         blocks = markdown_to_blocks(md)
         self.assertEqual(blocks, ["This just one big block with **bold** and _italics_ and a [link](https://www.url.com)"])
+
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading(self):
+        header1 = "# Heading 1"
+        header2 = "## Heading 2"
+        header3 = "### Heading 3"
+        header4 = "#### Heading 4"
+        header5 = "##### Heading 5"
+        header6 = "###### Heading 6"
+
+        self.assertEqual(block_to_block_type(header1), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(header2), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(header3), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(header4), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(header5), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(header6), BlockType.HEADING)
+
+    def test_heading_edge_cases(self):
+        heading7 = "####### Heading 7"
+        heading = "#Heading"
+        self.assertEqual(block_to_block_type(heading7), BlockType.PARAGRAPH)
+        self.assertEqual(block_to_block_type(heading), BlockType.PARAGRAPH)
+
+    def test_paragraph(self):
+        paragraph = "This is a paragraph of text."
+        self.assertEqual(block_to_block_type(paragraph), BlockType.PARAGRAPH)
+
+    def test_empty_string(self):
+        self.assertEqual(block_to_block_type(""), BlockType.PARAGRAPH)
+
+    def test_unordered_list(self):
+        unordered_list = "- Item 1\n- Item 2\n- Item 3"
+        self.assertEqual(block_to_block_type(unordered_list), BlockType.UNORDERED_LIST)
+
+    def test_unordered_list_edge_cases(self):
+        unordered_list = "- Item 1\nItem 2\n- Item 3"
+        self.assertEqual(block_to_block_type(unordered_list), BlockType.PARAGRAPH)
+
+    def test_ordered_list(self):
+        ordered_list = "1. Item 1\n2. Item 2\n3. Item 3"
+        self.assertEqual(block_to_block_type(ordered_list), BlockType.ORDERED_LIST)
+
+    def test_ordered_list_edge_cases(self):
+        ordered_list1 = "2. Item 1\n3. Item 2\n4. Item 3"
+        self.assertEqual(block_to_block_type(ordered_list1), BlockType.PARAGRAPH)
+        ordered_list2 = "1. Item 1\n3. Item 2\n5. Item 3"
+        self.assertEqual(block_to_block_type(ordered_list2), BlockType.PARAGRAPH)
+
+    def test_quotes(self):
+        quote1 = "> This is a quote."
+        quote2 = ">This is also a quote.\n> But longer."
+        self.assertEqual(block_to_block_type(quote1), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(quote2), BlockType.QUOTE)
+
+    def test_quotes_edge_cases(self):
+        quote = ">This is also a quote.\nBut longer.\n> And badly formated."
+        self.assertEqual(block_to_block_type(quote), BlockType.PARAGRAPH)
+
+    def test_code(self):
+        code = "```\n\nThis is code\n```"
+        self.assertEqual(block_to_block_type(code), BlockType.CODE)
+
+    def test_code_edge_case(self):
+        code1 = "```\n\nThis is endless code"
+        self.assertEqual(block_to_block_type(code1), BlockType.PARAGRAPH)
+        code = "```This is code on one line```"
+        self.assertEqual(block_to_block_type(code), BlockType.PARAGRAPH)
